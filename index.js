9fa6ff53017d2e779e95d73fdd1692eabee167a9
rishavratzee <rishavforu0123@gmail.com>
2024-06-14 04:05:55 +0530

    data types

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-let interestRate = 0.3;
-interestRate = 1;
-console.log(interestRate)
-
-
-// const interestRate = 0.3;
-// interestRate = 1;
-// console.log(interestRate)
+let name = 'Rishav'; // String Literal
+let age = 30; // Number Literal
+let isApproved = false; // Boolean Literal
+let firstName = undefined;
+let selectedColor = null;
