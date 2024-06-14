8600f58ee8ab408511e21f81d41c0c92f6cd7cfd
rishavratzee <rishavforu0123@gmail.com>
2024-06-14 09:22:41 +0530

    Facftory function

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-//for-of
 
-let colors = ['Red','Green','Blue'];
-for(let color of colors)
-    console.log(color);
+
+
+//Factory functions
+
+function createCircle(radius){
+    //Object Oriented Programming
+    return {
+        radius,
+        draw() {
+            console.log('draw');
+        }
+    };
+
+}
+
+const circle1 = createCircle(1);
+console.log(circle1)
+
+
