c5e011447c81781854bc54f9be8317decb9a69f6
rishavratzee <rishavforu0123@gmail.com>
2024-06-14 07:28:05 +0530

    dynamic typing

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ let name = 'Rishav'; // String Literal
 let age = 30; // Number Literal
 let isApproved = false; // Boolean Literal
 let firstName = undefined;
-let selectedColor = null;
+let selectedColor = null
+
+//Dynamic typing
+
+typeof(name);
+name = 1;
+typeof(name);
+
