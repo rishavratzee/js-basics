4a9e956180f69f6e1197273f853b304f091cf722
rishavratzee <rishavforu0123@gmail.com>
2024-06-14 08:27:51 +0530

    For in

--- index.js
+++ index.js
@@ -1,52 +1,7 @@
-//Operators
+//For..in loop
 
-let x = 30;  //assigment operator
+let items = ['Marry', 'Gold', "Hero"];
 
-x += 5; //shorthand operator
-x *= 3;
-
-//comparison operator
-
-//Relational operator
-console.log( x>0 );
-
-//Equality operator
-
-//strict equality operator
-console.log('1' === 1);
-
-//Lose Equality
-console.log( '1'==1 );
-
-console.log(x !== 1);
-
-
-//Ternanry operator
-
-let point = 110;
-let type  = ( point > 100) ? 'Gold' : 'Silver';
-console.log(type)
-
-//Logical operators
-
-//(&&) - Logical AND
-//(||) - Logical OR
-//( !) - Logical NOT
-
-
-//Falsy values - Treated as Boolean false
-//undefined
-//null
-//0
-//false
-//''
-//NaN
-
-//Anything that is not Falsy is Truthy
-
-
-
-
-
-
-
+for(let index in items){
+    console.log(items[index]);
+}
