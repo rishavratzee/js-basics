209764364d5470b9c43ff18379466040b45a2427
rishavratzee <rishavforu0123@gmail.com>
2024-06-14 07:42:07 +0530

    Types of function

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 //Functions
 
+
+//Performing a task
 function greet(message) {
     //body of the function
     console.log(message);
 }
 
+//Calculates a value
+function square(number){
+   return number * number;
+}
+
 greet('Hello from the function');
+let number = square(2);
+
+
+console.log(number);
