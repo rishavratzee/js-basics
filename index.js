c03ce94a4f58195c1707eef3f57c1ed37d2985cc
rishavratzee <rishavforu0123@gmail.com>
2024-06-14 08:01:23 +0530

    Operators extended

--- index.js
+++ index.js
@@ -18,9 +18,35 @@ console.log('1' === 1);
 //Lose Equality
 console.log( '1'==1 );
 
-
-
 console.log(x !== 1);
+
+
+//Ternanry operator
+
+let point = 110;
+let type  = ( point > 100) ? 'Gold' : 'Silver';
+console.log(type)
+
+//Logical operators
+
+//(&&) - Logical AND
+//(||) - Logical OR
+//( !) - Logical NOT
+
+
+//Falsy values - Treated as Boolean false
+//undefined
+//null
+//0
+//false
+//''
+//NaN
+
+//Anything that is not Falsy is Truthy
+
+
+
+
 
 
 
